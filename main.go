f034409523253ef928b6c3a1577f6b99aea0e9f0
maruo-t <t.maruo.dev@gmail.com>
2023-08-15 17:27:19 +0900

    関数

--- main.go
+++ main.go
@@ -1,49 +1,85 @@
 package main
 
-// i5 := 500 // エラー
-// s5 := "Golang" // エラー
-var i5 = 500
-var s5 = "Golang"
+func Plus(x, y int) int {
+	return x + y
+}
 
-func outer() {
-	// i6 := 600 // エラー
-	// s6 := "Golang" // エラー
-	var i6 = 600
-	var s6 = "Golang"
-	println(i6, s6)
+func Divide(x, y int) (int, int) {
+	q := x / y
+	r := x % y
+	return q, r
+}
+
+func Double(price int) (result int) {
+	result = price * 2
+	return
+}
+
+func Noreturn() {
+	println("No return")
+	return
+}
+
+func returnFunc() func() {
+	return func() {
+		println("I'm a function")
+	}
+}
+
+func incrementGenerator() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}
 }
 
 func main() {
-	println("Hello, World!")
-	var i int = 100
+	i := Plus(1, 2)
 	println(i)
 
-	var s string = "Hello, World!"
-	println(s)
+	i2, _ := Divide(9, 2)
+	println(i2)
 
-	var t, f bool = true, false
-	println(t, f)
+	i4 := Double(1000)
+	println(i4)
 
-	var (
-		i2 int    = 200
-		s2 string = "Golang"
-	)
-	println(i2, s2)
+	Noreturn()
 
-	var i3 int
-	var s3 string
-	println(i3, s3)
+	// 無名関数
+	f := func(x, y int) int { return x + y }
+	i5 := f(1, 2)
+	println(i5)
 
-	i3 = 300
-	s3 = "Golang"
-	println(i3, s3)
+	i6 := func(x, y int) int { return x + y }(1, 2)
+	println(i6)
 
-	// 暗黙的な定義
-	i4 := 400
-	s4 := "Golang"
-	println(i4, s4)
+	// 関数を返す関数
+	f2 := returnFunc()
+	f2()
 
-	println(i5, s5)
+	// クロージャ
+	counter1 := incrementGenerator()
+	counter2 := incrementGenerator()
+	println(counter1())
+	println(counter1())
+	println(counter2())
+	println(counter2())
+	println(counter2())
+	println(counter1())
 
-	outer()
+	// ジェネレーター
+	gen := func() func() int {
+		x := 0
+		return func() int {
+			x++
+			return x
+		}
+	}
+	counter3 := gen()
+	println(counter3())
+	println(counter3())
+	println(counter3())
+	println(counter3())
+	println(counter3())
 }
